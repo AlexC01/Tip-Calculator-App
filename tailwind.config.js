4e9e9b0e90ca270ebc8fa2187584de440da5e177
AlexC01 <alexcuadraxxx@gmail.com>
2021-07-11 19:40:13 -0600

    Inicializando tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      mono: ['Space Mono', 'monospace'],
+    },
+    extend: {
+      colors: {
+        primary: '#c5e4e7',
+        secondary: '#00494d',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
